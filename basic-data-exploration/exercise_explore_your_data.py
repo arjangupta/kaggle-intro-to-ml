386b1159c0bc91dc84b39de22e8ebf2c8af09657
Arjan Gupta <arjangupta95@gmail.com>
2020-12-06 14:36:26 -0600

    Open the iowa data csv

---
+++ basic-data-exploration/exercise_explore_your_data.py
@@ -0,0 +1,17 @@
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex2 import *
+print("Setup Complete")
+
+import pandas as pd
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+# Fill in the line below to read the file into a variable home_data
+home_data = pd.read_csv(iowa_file_path);
+home_data.describe();
+
+# Call line below with no argument to check that you've loaded the data correctly
+step_1.check()
--- basic_data_exploration/exercise_explore_your_data.py
+++
@@ -1,5 +0,0 @@
-# Set up code checking
-from learntools.core import binder
-binder.bind(globals())
-from learntools.machine_learning.ex2 import *
-print("Setup Complete")
